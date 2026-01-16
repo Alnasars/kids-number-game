b915b695d41ad8826b294cd019f633e70101e569
Alnasars <alnasars@gmail.com>
2026-01-16 19:39:20 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,37 +1,23 @@
-const cacheName = 'math-v3'; // Incremented to v3 for the new features
+const cacheName = 'math-v16'; 
 const assets = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon.png',
+  '/kids-number-game/',
+  '/kids-number-game/index.html',
+  '/kids-number-game/manifest.json',
+  '/kids-number-game/icon.png',
   'https://cdnjs.cloudflare.com/ajax/libs/tone/14.8.49/Tone.min.js'
 ];
 
-// Install: Saves the files to the phone's memory
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      console.log('Brain: Saving new game files...');
-      return cache.addAll(assets);
-    })
-  );
-  self.skipWaiting(); // Forces the new version to start immediately
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+  self.skipWaiting();
 });
 
-// Activate: Deletes old versions of the game
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== cacheName).map(key => caches.delete(key))
-      );
-    })
-  );
+  e.waitUntil(caches.keys().then(keys => {
+    return Promise.all(keys.filter(key => key !== cacheName).map(key => caches.delete(key)));
+  }));
 });
 
-// Fetch: Allows the game to work without internet
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
