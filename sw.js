ed0e48429da781fe6f0cd2d7e5cdd24ace8fe432
Alnasars <alnasars@gmail.com>
2026-02-02 17:49:03 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,46 +1,28 @@
-const CACHE_NAME = 'kids-math-v1.4';
+const CACHE_NAME = 'kids-math-v2.0';
 const ASSETS = [
   '/',
   '/index.html',
-  '/icon.png',
-  'https://cdn.tailwindcss.com',
-  'https://cdnjs.cloudflare.com/ajax/libs/tone/14.8.49/Tone.min.js'
+  '/icon.png'
 ];
 
-// Install & Cache
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
-  self.skipWaiting();
 });
 
-// Clean old versions
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) return caches.delete(key);
-        })
-      );
+      return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
     })
   );
 });
 
-// Stale-While-Revalidate (Stable Loading)
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      const fetchedResponse = fetch(event.request).then((networkResponse) => {
-        return caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, networkResponse.clone());
-          return networkResponse;
-        });
-      });
-      return cachedResponse || fetchedResponse;
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
